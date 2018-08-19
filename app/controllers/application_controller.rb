0a6c2940aa21045ffb625f8a9606b3588a15a7bf
Juan <juanbedon26@hotmail.com>
2018-08-19 14:20:39 -0500

    Integrated devise within API

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 class ApplicationController < ActionController::Base
+
   protect_from_forgery with: :exception
   #before_action :authenticate_user!
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, token|
+      user = User.find_by_email(username)
+      if user.auth_token == token
+        sign_in user
+      end
+    end
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -18,9 +18,13 @@
 #
 
 class User < ApplicationRecord
+	
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
   has_many :expenses
+  has_secure_token :auth_token
+
 end
