12958af001124576cba612d6eb1c0b9bb7474744
Juan <juanbedon26@hotmail.com>
2018-08-20 17:41:10 -0500

    Applied refactor to index method in api controller

--- app/controllers/api/v1/expenses_controller.rb
+++ app/controllers/api/v1/expenses_controller.rb
@@ -2,27 +2,10 @@ class Api::V1::ExpensesController < ApplicationController
 
   def index
 
-    transaction_type = params["transaction_type"]
-    category = params[:category]
-
-    if transaction_type && !category
-      @expenses = Expense.joins(:transaction_type).where("transaction_types.name = ?", transaction_type.capitalize)
-    elsif category && !transaction_type
-      @expenses = Expense.joins(:category).where("categories.name = ?", category.capitalize)
-    elsif category && transaction_type
-      @expenses = Expense.joins(:transaction_type, :category).where("transaction_types.name = ? AND categories.name = ?",
-      transaction_type.capitalize, category.capitalize)
-    else
-      @expenses = Expense.all
-    end
-
-    @transaction_types = TransactionType.all
-    @category = Category.all
+    @expenses = Expense.all
 
     respond_to do |format|
-      format.html { render :index}
       format.json { render json: @expenses, status: :ok}
-      format.js { render :index}
     end
 
   end
