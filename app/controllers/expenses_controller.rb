5da79817fdaed2bb11b246cf229de9d1ddaafae6
Juan <juanbedon26@hotmail.com>
2018-08-25 14:01:03 -0500

    Dropdown menu is working!

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -20,12 +20,47 @@ class ExpensesController < ApplicationController
     @category = Category.all
 
     current_month = params["current_month"]
+    month_ago = params["month_ago"]
+    two_months_ago = params["two_months_ago"]
+    three_months_ago = params["three_months_ago"]
+    four_months_ago = params["four_months_ago"]
+    five_months_ago = params["five_months_ago"]
+    six_months_ago = params["six_months_ago"]
+    seven_months_ago = params["seven_months_ago"]
+    eight_months_ago = params["eight_months_ago"]
+    nine_months_ago = params["nine_months_ago"]
+    ten_months_ago = params["ten_months_ago"]
+    eleven_months_ago = params["eleven_months_ago"]
+    twelve_months_ago = params["twelve_months_ago"]
 
     if current_month
-      byebug
-      @expenses = Expense.where(date: Date.current.strftime("%Y-%m-%d"))
-    else
       @expenses = Expense.where(date: 0.months.ago.all_month)
+    elsif month_ago
+      @expenses = Expense.where(date: 1.month.ago.all_month)
+    elsif two_months_ago
+      @expenses = Expense.where(date: 2.months.ago.all_month)
+    elsif three_months_ago
+      @expenses = Expense.where(date: 3.months.ago.all_month)
+    elsif four_months_ago
+      @expenses = Expense.where(date: 4.months.ago.all_month)
+    elsif five_months_ago
+      @expenses = Expense.where(date: 5.months.ago.all_month)
+    elsif six_months_ago
+      @expenses = Expense.where(date: 6.months.ago.all_month)
+    elsif seven_months_ago
+      @expenses = Expense.where(date: 7.months.ago.all_month)
+    elsif eight_months_ago
+      @expenses = Expense.where(date: 8.months.ago.all_month)
+    elsif nine_months_ago
+      @expenses = Expense.where(date: 9.months.ago.all_month)
+    elsif ten_months_ago
+      @expenses = Expense.where(date: 10.months.ago.all_month)
+    elsif eleven_months_ago
+      @expenses = Expense.where(date: 11.months.ago.all_month)
+    elsif twelve_months_ago
+      @expenses = Expense.where(date: 12.months.ago.all_month)
+    else
+      @expenses = Expense.all
     end
 
     respond_to do |format|
