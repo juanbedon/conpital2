f62b958a4dc057b9df1d2e7aa5853875a27a7834
Juan <juanbedon26@hotmail.com>
2018-08-26 15:07:30 -0500

    Installed groupdate gem, added @ to DashboardController and modified charts

--- Gemfile
+++ Gemfile
@@ -41,6 +41,7 @@ gem 'jquery-rails'
 gem 'faker'
 gem 'annotate'
 gem 'chartkick'
+gem 'groupdate'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,11 +1,11 @@
 class DashboardController < ApplicationController
 
   def index
-    handler = DashboardHandler.new(Expense.all)
-    @amount_of_today = handler.amount_of_today
-    @amount_of_yesterday = handler.amount_of_yesterday
-    @amount_of_current_month = handler.amount_of_current_month
-    @amount_of_last_month = handler.amount_of_last_month
+    @handler = DashboardHandler.new(Expense.all)
+    @amount_of_today = @handler.amount_of_today
+    @amount_of_yesterday = @handler.amount_of_yesterday
+    @amount_of_current_month = @handler.amount_of_current_month
+    @amount_of_last_month = @handler.amount_of_last_month
   end
   
 end
