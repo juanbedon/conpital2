22b7347f5a39b5de29030e6e844dbfcaf9af179f
Juan <juanbedon26@hotmail.com>
2018-09-04 16:43:43 -0500

    Modified where it should redirect to

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,7 +16,7 @@ class ApplicationController < ActionController::Base
 
   # Overwriting the sign_out redirect path method
   def after_sign_out_path_for(resource_or_scope)
-    root_path
+    dashboard_index_path
   end
 
 end
