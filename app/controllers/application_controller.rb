a6c0a4786f762b2835bb673aedd8f3cbc284030a
Juan <juanbedon26@hotmail.com>
2018-09-04 14:42:24 -0500

    Commented befor_action in app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
 
   protect_from_forgery with: :exception
-  before_action :authenticate_user!
+  #before_action :authenticate_user!
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, token|
