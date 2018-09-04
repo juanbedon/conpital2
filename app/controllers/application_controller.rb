69a8c651c74029d520c65a9605b5856b99943f2f
Juan <juanbedon26@hotmail.com>
2018-09-04 17:02:37 -0500

    Let's see if it works now

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,11 +12,4 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  private
-
-  # Overwriting the sign_out redirect path method
-  def after_sign_out_path_for(resource_or_scope)
-    redirect_to root_path
-  end
-
 end
