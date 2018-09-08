7f8c6d85c656da267721b8e8ad69cd47b540641b
Juan <juanbedon26@hotmail.com>
2018-09-08 13:28:18 -0500

    did not work

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -97,12 +97,4 @@ config.webpacker.check_yarn_integrity = false
 
   config.reload_controllers = true
 
-  if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
-  }
-
-  app.get('*', (request, response) => {
-    response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-  });
-
 end
