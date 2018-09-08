4ae22b7ed21adc0207b28ceee7009b9045d43c29
Juan <juanbedon26@hotmail.com>
2018-09-08 13:09:11 -0500

    Trying api to work in Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,4 +95,6 @@ config.webpacker.check_yarn_integrity = false
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.reload_controllers = true
+
 end
