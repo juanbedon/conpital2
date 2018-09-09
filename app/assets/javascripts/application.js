e0c2760921ae84a384a43a637047619ed97673da
Juan <juanbedon26@hotmail.com>
2018-09-08 20:12:15 -0500

    Changed order

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
 //= require Chart.bundle
 //= require chartkick
 //= require turbolinks
 //= require bootstrap
+//= require jquery
+//= require jquery_ujs
 //= require_tree .
